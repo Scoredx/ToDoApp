3e9cdbe93f0d1453d58e4c783914b852717dd32d
Scored <multiusage2@o2.pl>
2020-03-24 20:06:08 +0100

    Final

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,26 @@
-const TodoContainer = document.querySelectorAll('.todo_item_container')[0];
+
+
+const TodoContainer = document.querySelectorAll('.todo_container')[0];
 const TodoAdd = document.querySelectorAll('.todo_plus')[0];
  
 TodoAdd.addEventListener('click', () => {
   TodoContainer.appendChild(genTodoItem());
 });
  
-function ToDo() {
+function genTodoItem() {
   let cont = document.createElement("div");
   cont.className = "todo_item";
   let text = document.createElement("span");
-  text.className = "todo_item_text";
-  text.textContent = "Don't forget about ...";
+  text.className = "todo_text";
+  var newtext = (text.textContent = prompt('Write what you need...', text.textContent))
+  text.textContent = newtext;
   let controls = document.createElement('span');
   controls.className = 'todo_controls';
   let edit = document.createElement('img');
-  edit.src = 'assets/icons/more.svg';
-  edit.onclick = () => (text.textContent = prompt('Write what you need...', text.textContent));
+  edit.src = './assets/icons/more.svg';
+  edit.onclick = () => (text.textContent = prompt(newtext, text.textContent));
   let remove = document.createElement('img');
-  remove.src = 'assets/icons/check.svg';
+  remove.src ='./assets/icons/check.svg';
   remove.onclick = () => TodoContainer.removeChild(cont);
   controls.appendChild(edit);
   controls.appendChild(remove);
@@ -25,3 +28,7 @@ function ToDo() {
   cont.appendChild(controls);
   return cont;
 }
+
+
+
+
