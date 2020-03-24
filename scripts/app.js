68bb0497a084a5fb8685f7e8003ae18f163de0bb
Scored <multiusage2@o2.pl>
2020-03-24 20:26:54 +0100

    final

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,24 @@
-
-
-const TodoContainer = document.querySelectorAll('.todo_container')[0];
+// variables
+const TodoBox = document.querySelectorAll('.todo_container')[0];
 const TodoAdd = document.querySelectorAll('.todo_plus')[0];
  
+//new task button
 TodoAdd.addEventListener('click', () => {
-  TodoContainer.appendChild(genTodoItem());
+  TodoBox.appendChild(genTodoItem());
 });
  
 function genTodoItem() {
+
+  //Container + Text
   let cont = document.createElement("div");
   cont.className = "todo_item";
   let text = document.createElement("span");
   text.className = "todo_text";
-  var newtext = (text.textContent = prompt('Write what you need...', text.textContent))
+  var newtext = (text.textContent = prompt('Type here! ', text.textContent))
   text.textContent = newtext;
+
+
+  //Buttons
   let controls = document.createElement('span');
   controls.className = 'todo_controls';
   let edit = document.createElement('img');
@@ -21,7 +26,7 @@ function genTodoItem() {
   edit.onclick = () => (text.textContent = prompt(newtext, text.textContent));
   let remove = document.createElement('img');
   remove.src ='./assets/icons/check.svg';
-  remove.onclick = () => TodoContainer.removeChild(cont);
+  remove.onclick = () => TodoBox.removeChild(cont);
   controls.appendChild(edit);
   controls.appendChild(remove);
   cont.appendChild(text);
