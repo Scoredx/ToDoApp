485a0411b3cbd1bcd2b3def0ecd56bd0625bb2db
Scoredx <multiusage2@o2.pl>
2020-06-22 23:10:46 +0200

    Final

--- scripts/app.js
+++ scripts/app.js
@@ -1,39 +1,35 @@
-// variables
-const TodoBox = document.querySelectorAll(".todo_container")[0];
-const TodoAdd = document.querySelectorAll(".todo_plus")[0];
- 
-//new task button
-TodoAdd.addEventlistener("click", () => {
-  TodoBox.appendChild(genTodoItem());
+//get obj from HTML
+const TodoContainer = document.querySelectorAll('.todo_container')[0];
+const TodoAdd = document.querySelectorAll('.todo_plus')[0];
+
+ //new task button
+TodoAdd.addEventListener('click', () => {
+  TodoContainer.appendChild(genTodoItem());
 });
  
+//Button action
 function genTodoItem() {
 
-  //Container + Text
-  let cont = document.createElement("div");
-  cont.className = "todo_item";
-  let text = document.createElement("span");
-  text.className = "todo_text";
-  var newtext = (text.textContent = prompt("Type here! ", text.textContent))
-  text.textContent = newtext;
+	//Container + Text
+	let cont = document.createElement("div");
+	cont.className = "todo_item";
+	let text = document.createElement("span");
+	text.className = "todo_item_text";
+	var input = window.prompt("Enter text: ");
+	text.textContent = input;
 
-
-  //Buttons
-  let controls = document.createElement("span");
-  controls.className = "todo_controls";
-  let edit = document.createElement("img");
-  edit.src = "./assets/icons/more.svg";
-  edit.onclick = () => (text.textContent = prompt(newtext, text.textContent));
-  let remove = document.createElement("img");
-  remove.src ="./assets/icons/check.svg";
-  remove.onclick = () => TodoBox.removeChild(cont);
-  controls.appendChild(edit);
-  controls.appendChild(remove);
-  cont.appendChild(text);
-  cont.appendChild(controls);
-  return cont;
+	//Controls 
+	let controls = document.createElement('span');
+	controls.className = 'todo_controls';
+	let edit = document.createElement('img');
+	edit.src = "./assets/icons/more.svg";
+	edit.onclick = () => (text.textContent = prompt('Edit: ', text.textContent));
+	let remove = document.createElement('img');
+	remove.src ="./assets/icons/check.svg";
+	remove.onclick = () => TodoContainer.removeChild(cont);
+	controls.appendChild(edit);
+	controls.appendChild(remove);
+	cont.appendChild(text);
+	cont.appendChild(controls);
+	return cont;
 }
-
-
-
-
